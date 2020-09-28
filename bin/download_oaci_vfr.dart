ad7d685c5273f758c4e4e26e8a24a384d76283ad
CaramelDunes <dev@carameldunes.fr>
2020-09-28 22:19:08 +0200

    Clean-up the download_oaci_vfr script.

--- bin/download_oaci_vfr.dart
+++ bin/download_oaci_vfr.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class UserAgentClient extends http.BaseClient {
+  static final String firefoxUserAgent =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0';
+
   final String userAgent;
   final http.Client _inner;
 
@@ -15,97 +18,51 @@ class UserAgentClient extends http.BaseClient {
 }
 
 Future<bool> downloadTile(http.Client client, int x, int y, int zoom) async {
-  var path = 'tiles/$zoom/$y-$x.jpg';
+  var directory = Directory('assets/offline_layers/oaci_vfr/$zoom/');
+  directory.createSync(recursive: true);
 
-  var file = File(path);
+  var file = File(directory.path + '/$y-$x.jpg');
 
   if (!await file.exists()) {
     String url =
         'https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-OACI&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image/jpeg&TileMatrix=$zoom&TileCol=$y&TileRow=$x';
 
-    print('$path dling');
-    var r = await client.get(url);
+    var response = await client.get(url);
 
-    if (r.statusCode == 200) {
-      print('$path 200');
+    if (response.statusCode == 200) {
+      print('Downloaded $zoom/$x-$y.');
 
-      await file.writeAsBytes(r.bodyBytes);
-      return Future.value(true);
+      await file.writeAsBytes(response.bodyBytes);
+      return true;
     } else {
-      print(url);
-      return Future.value(false);
+      print('Error downloading $zoom/$x-$y.');
+      return false;
     }
   } else {
-    // print('tiles/$y-$x.jpg exists');
-    return Future.value(true);
+    return true;
   }
 }
 
-Future dl11() async {
-  String ua =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0';
-
-  var cl = UserAgentClient(ua, http.Client());
-
-  List<Future> toWait = [];
-
-  for (int x = 684; x < 770; x++) {
-    for (int y = 993; y < 1080; y++) {
-      await downloadTile(cl, x, y, 11);
+Future downloadLayer(http.Client client, int zoomLevel, int fromX, int toX,
+    int fromY, int toY) async {
+  for (int x = fromX; x < toX; x++) {
+    for (int y = fromY; y < toY; y++) {
+      await downloadTile(client, x, y, zoomLevel);
     }
   }
-
-  await Future.wait(toWait);
-
-  cl.close();
 }
 
-Future dl8() async {
-  String ua =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0';
+Future<int> main(List<String> arguments) async {
+  http.Client innerClient = http.Client();
+  UserAgentClient client =
+      UserAgentClient(UserAgentClient.firefoxUserAgent, innerClient);
 
-  var cl = UserAgentClient(ua, http.Client());
+  await downloadLayer(client, 8, 85, 97, 123, 137);
+  await downloadLayer(client, 9, 170, 194, 247, 273);
+  await downloadLayer(client, 10, 340, 387, 494, 545);
+  await downloadLayer(client, 11, 684, 770, 993, 1080);
 
-  for (int x = 85; x < 97; x++) {
-    for (int y = 123; y < 137; y++) {
-      await downloadTile(cl, x, y, 8);
-    }
-  }
+  innerClient.close();
 
-  cl.close();
-}
-
-Future dl10() async {
-  String ua =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0';
-
-  var cl = UserAgentClient(ua, http.Client());
-
-  for (int x = 340; x < 387; x++) {
-    for (int y = 494; y < 545; y++) {
-      await downloadTile(cl, x, y, 10);
-    }
-  }
-
-  cl.close();
-}
-
-Future dl9() async {
-  String ua =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0';
-
-  var cl = UserAgentClient(ua, http.Client());
-
-  for (int x = 170; x < 194; x++) {
-    for (int y = 247; y < 273; y++) {
-      await downloadTile(cl, x, y, 9);
-    }
-  }
-
-  cl.close();
-}
-
-Future main(List<String> arguments) async {
-  await dl10();
   return 0;
 }
